43d6f3df3fc123fcb98b1d4e4fb296b3257c0ecd
Francesco Sciabbarrasi <francesco.sciabbarrasii@gmail.com>
2024-10-08 12:25:40 +0100

    Implementing try catch and testing first run. To change file selection.

--- src/promoapplier/PromoApplier.java
+++ src/promoapplier/PromoApplier.java
@@ -8,14 +8,30 @@ package promoapplier;
  *
  * @author francescosciabbarrasi
  */
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 public class PromoApplier {
 
     /**
      * @param args the command line arguments
      */
+    
     public static void main(String[] args) {
         // TODO code application logic here
-        //Test 2
+        try {
+            File customers = new File("customers.txt");
+            Scanner customersData = new Scanner(new FileReader(customers));
+            while (customersData.hasNextLine()) {
+                String data = customersData.nextLine();
+                System.out.println(data);
+        }
+        } catch (FileNotFoundException e){
+                System.out.println("An error occurred.");
+        }
+        }
+                
     }
     
-}
+
